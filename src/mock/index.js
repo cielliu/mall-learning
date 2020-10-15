65cc18f47e177725fd9416ded864be8c6989cf73
cielliu <ljnbupt@gmail.com>
2020-10-15 22:01:01 +0800

    mock-test

--- src/mock/index.js
+++ src/mock/index.js
@@ -4,7 +4,7 @@ import Mock from 'mockjs'
 import login from './modules/login'
 
 // Mock函数
-const  mocks = [
+const mocks = [
   ...login
 ]
 // // 设置延时
@@ -14,59 +14,57 @@ const  mocks = [
 
 // // 抄来一个解析地址栏参数解析函数
 export function param2Obj (url) {
-    const search = url.split('?')[1]
-    if (!search) {
-        return {}
-    }
-    return JSON.parse(
-        '{"' +
+  const search = url.split('?')[1]
+  if (!search) {
+    return {}
+  }
+  return JSON.parse(
+    '{"' +
         decodeURIComponent(search)
-            .replace(/"/g, '\\"')
-            .replace(/&/g, '","')
-            .replace(/=/g, '":"')
-            .replace(/\+/g, ' ') +
+          .replace(/"/g, '\\"')
+          .replace(/&/g, '","')
+          .replace(/=/g, '":"')
+          .replace(/\+/g, ' ') +
         '"}'
-    )
+  )
 }
 
 // 关键！抄来一个前端模式构建函数（或者你也可以建一个mock server）
-export function mockXHR() {
-    // mock patch
-    // https://github.com/nuysoft/Mock/issues/300
-    Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send
-    Mock.XHR.prototype.send = function() {
-      if (this.custom.xhr) {
-        this.custom.xhr.withCredentials = this.withCredentials || false
-  
-        if (this.responseType) {
-          this.custom.xhr.responseType = this.responseType
-        }
-      }
-      this.proxy_send(...arguments)
-    }
-  
-    function XHR2ExpressReqWrap(respond) {
-      return function(options) {
-        let result = null
-        if (respond instanceof Function) {
-          const { body, type, url } = options
-          // https://expressjs.com/en/4x/api.html#req
-          result = respond({
-            method: type,
-            body: JSON.parse(body),
-            query: param2Obj(url)
-          })
-        } else {
-          result = respond
-        }
-        return Mock.mock(result)
+export function mockXHR () {
+  // mock patch
+  // https://github.com/nuysoft/Mock/issues/300
+  Mock.XHR.prototype.proxy_send = Mock.XHR.prototype.send
+  Mock.XHR.prototype.send = function () {
+    if (this.custom.xhr) {
+      this.custom.xhr.withCredentials = this.withCredentials || false
+
+      if (this.responseType) {
+        this.custom.xhr.responseType = this.responseType
       }
     }
-  
-    for (const i of mocks) {
-     Mock.mock(new RegExp(i.url), i.type || 'get', XHR2ExpressReqWrap(i.response))
-      //Mock.mock(new RegExp(i.url), i.type, login)
+    this.proxy_send(...arguments)
+  }
+
+  function XHR2ExpressReqWrap (respond) {
+    return function (options) {
+      let result = null
+      if (respond instanceof Function) {
+        const { body, type, url } = options
+        // https://expressjs.com/en/4x/api.html#req
+        result = respond({
+          method: type,
+          body: JSON.parse(body),
+          query: param2Obj(url)
+        })
+      } else {
+        result = respond
+      }
+      return Mock.mock(result)
     }
   }
 
- 
+  for (const i of mocks) {
+    Mock.mock(new RegExp(i.url), i.type || 'get', XHR2ExpressReqWrap(i.response))
+    // Mock.mock(new RegExp(i.url), i.type, login)
+  }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld'
+// 定义组件
 import login from '@/views/login'
 Vue.use(Router)
 
+// 定义路由映射
+
+// 创建router实例,
 export default new Router({
   routes: [
     {
